df238c4fbba667ecbf1fdd4f92e0b42be1e8e77a
Dante Nardo <dantenardo@Dantes-MacBook-Air.local>
2022-01-16 16:13:21 -0800

    Fixed netlify adapter

--- protean-blueprint/svelte.config.js
+++ protean-blueprint/svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +11,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: netlify(),
+		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
